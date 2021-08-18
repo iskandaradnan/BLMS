f54e64437f0558957747358be27013eeaed027a6
iskandar <iskandar.adnan@edgenta.com>
2021-08-18 18:37:12 +0800

    Create AuditLogController

---
+++ Controllers/AuditLogController.cs
@@ -0,0 +1,22 @@
+﻿using BLMS.v2.Context;
+using BLMS.v2.Models.Admin;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLMS.v2.Controllers
+{
+    public class AuditLogController : Controller
+    {
+        readonly AuditLogDbContext dbContext = new AuditLogDbContext();
+
+        public IActionResult Index()
+        {
+            List<AuditLog> AuditLogList = dbContext.GetAuditLog().ToList();
+
+            return View(AuditLogList);
+        }
+    }
+}
