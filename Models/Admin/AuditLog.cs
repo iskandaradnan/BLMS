dcb343c1cf8aaf4f2baea5fc115326239ae17996
iskandar <iskandar.adnan@edgenta.com>
2021-08-18 18:06:35 +0800

    Create AuditLog model

---
+++ Models/Admin/AuditLog.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLMS.v2.Models.Admin
+{
+    public class AuditLog
+    {
+        public int ID { get; set; }
+
+        public string Command { get; set; }
+
+        public string SPName { get; set; }
+
+        public string ScreenPath { get; set; }
+
+        public string OldValue { get; set; }
+
+        public string NewValue { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string CreatedDt { get; set; }
+    }
+}
